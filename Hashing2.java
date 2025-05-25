74ab543bba0dc6e85f7239b711155aa5b5b0f2fc
inder1304 <ikaur2@scu.edu>
2025-05-24 19:08:46 -0700

    Hashing2 done

--- Hashing2.java
+++ Hashing2.java
@@ -99,5 +99,22 @@ return maxLength;
 }*/
 
 
-
+class Solution {
+    public int longestPalindrome(String s) {
+        HashSet<Character> set = new HashSet<>();
+        int count = 0;
+        for(int i = 0; i < s.length(); i ++){
+            char ch = s.charAt(i);
+            if(set.contains(ch)){
+                count += 2;
+                set.remove(ch);
+            }
+            else
+                set.add(ch);
+        }
+        if(!set.isEmpty())
+            count++;
+        return count;
+    }
+}
 
